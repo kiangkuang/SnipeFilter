e378c7944487b9d87209eb8ea8f53c5f0283d4e3
Kiang Kuang <kiangkuang@gmail.com>
2016-08-20 02:36:29 +0800

    3 column filter list

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ $(document).ready(function() {
 
 var list = ["Aerodactyl", "Alakazam", "Arcanine", "Blastoise", "Chansey", "Charizard", "Dewgong", "Dragonair", "Dragonite", "Dugtrio", "Exeggutor", "Farfetch'd", "Flareon", "Gengar", "Grimer", "Gyarados", "Haunter", "Hitmonchan", "Hitmonlee", "Jolteon", "Kangaskhan", "Lapras", "Lickitung", "Magneton", "Marowak", "Mr. Mime", "Muk", "Nidoking", "Nidoqueen", "Ninetales", "Porygon", "Raichu", "Rapidash", "Slowbro", "Snorlax", "Vaporeon", "Venusaur", "Wigglytuff"];
 
-list.forEach(function(item) {
-    $("#filters").append(`
-        <p style="margin: 0;">
-            <input type="checkbox" name="filter" id="` + item + `" value="` + item + `" checked="checked"/>
-            <label for="` + item + `">` + item + `</label>
-        </p>
-    `);
-});
+for (var col = 0; col < 3; col++) {
+    for (var i = col * (list.length / 3); i < (col + 1) * (list.length / 3); i++) {
+        i = Math.ceil(i);
+        $($("#filters .col")[col]).append(`
+            <p style="margin: 0;">
+                <input type="checkbox" name="filter" id="` + list[i] + `" value="` + list[i] + `" checked="checked"/>
+                <label for="` + list[i] + `">` + list[i] + `</label>
+            </p>
+        `);
+    }
+}
 
 var interval = setInterval(getJson, 1000);
 
@@ -48,8 +51,8 @@ function parse(pokemon) {
                 <span class="title"><strong>` + pokemon.name + `</strong></span>
                 <p>
                     <i class="material-icons tiny">place</i> <a href="https://maps.google.com/?q=` + pokemon.coords + ` (` + pokemon.name + `)" target="_blank">` + pokemon.coords + `</a>
-                    <br><i class="material-icons tiny">timer</i> ` + 
-                    ~~((until - new Date()) / 60000) + `m ` + 
+                    <br><i class="material-icons tiny">timer</i> ` +
+                    ~~((until - new Date()) / 60000) + `m ` +
                     ~~(((until - new Date()) % 60000) / 1000) + `s
                 </p>
             </li>
